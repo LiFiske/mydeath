b496ea0d0e0643f88dd995c963e5f74824749024
LCF <lcfisk@gmail.com>
2025-05-06 23:13:31 -0700

    STyle: applied color scheme

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const html = `
   <html>
     <head>
       <link href="https://fonts.googleapis.com/css2?family=UnifrakturCook:wght@700&display=swap" rel="stylesheet">
-      <style>
+     <style>
   @font-face {
     font-family: 'UnifrakturCook';
     src: url('/fonts/UnifrakturCook.ttf') format('truetype');
@@ -11,21 +11,36 @@ const html = `
   }
 
   body {
+    background-color: #0c0c0c;
+    color: #d0d0d0;
+    font-family: 'Courier New', monospace;
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     flex-direction: column;
     text-align: center;
+    padding: 2rem;
   }
 
   h1 {
-    font-family: 'UnifrakturCook', serif;
-    font-size: 3em;
+    font-family: 'UnifrakturCook', monospace;
+    font-size: 2.8em;
+    color: #ff2f6e;
     margin-bottom: 0.5em;
+    text-shadow: 0 0 4px #ff2f6e88;
+  }
+
+  p {
+    margin: 0.4em 0;
+    font-size: 1.1em;
+    color: #d0d0d0;
+  }
+
+  strong {
+    color: #aaffcc;
   }
 </style>
-
     </head>
     <body>
       <h1>${name}'s Death Certificate</h1>
